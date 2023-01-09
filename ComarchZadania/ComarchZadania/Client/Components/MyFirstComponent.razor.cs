f59b352bfab2dd5405c401ad39ec2b9c0b6711ee
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2023-01-09 14:46:33 +0100

    Two-way binding

--- ComarchZadania/ComarchZadania/Client/Components/MyFirstComponent.razor.cs
+++ ComarchZadania/ComarchZadania/Client/Components/MyFirstComponent.razor.cs
@@ -6,4 +6,13 @@ public partial class MyFirstComponent : ComponentBase
 {
 	[Parameter]
 	public int CurrentCounterValue { get; set; }
+	
+	[Parameter]
+	public EventCallback<int> CurrentCounterValueChanged { get; set; }
+
+	async void UpdateCurrentCounterValue()
+	{
+		CurrentCounterValue++;
+		await CurrentCounterValueChanged.InvokeAsync(CurrentCounterValue);
+	}
 }
